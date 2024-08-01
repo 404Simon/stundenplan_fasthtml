d3ae775ca60036b5303b791777777c50a47d20be
404Simon <i22038@hb.dhbw-stuttgart.de>
2024-08-01 21:10:45 +0200

    add table entries

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fasthtml.common import *
 import random
+from datetime import datetime
 
 tailwind = Script(src="https://cdn.tailwindcss.com"),
 pico = Link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"),
@@ -31,38 +32,49 @@ rt = app.route
 
 @rt("/")
 async def get():
-    return Title("Stundenplan"), Main(H1("Stundenplan", cls="text-5xl m-4"), WeekTable())
+    return Title("Stundenplan"), Main(H1("Stundenplan", cls="text-5xl m-4"), Div(Navigation(), WeekTable()), cls="space-y-4")
 
 @rt("/table")
 async def post(weeks_from_now: int):
     print(f"Weeks from now: {weeks_from_now}")
-    return WeekTable(weeks_from_now)
+    return WeekTable()
 
-def render_table():
+
+def WeekTable():
     return Table(
         Tr(Th("Montag"), Th("Dienstag"), Th("Mittwoch"), Th("Donnerstag"), Th("Freitag")),
-        Tr(Td(random.randint(1,12)), Td("2"), Td("3"), Td("4"), Td("5")),
-        Tr(Td("6"), Td("7"), Td("8"), Td("9"), Td("10")),
-        Tr(Td("11"), Td("12"), Td("13"), Td("14"), Td("15")),
-        Tr(Td("16"), Td("17"), Td("18"), Td("19"), Td("20")),
-        cls="m-4"
+        Tr(random_table_entry(), random_table_entry(), random_table_entry(), random_table_entry(), random_table_entry()),
+        Tr(random_table_entry(), random_table_entry(), random_table_entry(), random_table_entry(), random_table_entry()),
+        Tr(random_table_entry(), random_table_entry(), random_table_entry(), random_table_entry(), random_table_entry()),
+        Tr(random_table_entry(), random_table_entry(), random_table_entry(), random_table_entry(), random_table_entry()),
+        cls="m-4",
+        id="stundenplan"
     )
 
 def Navigation(weeks_from_now=0):
     return Div(
         Button("<", cls="w-6 h-8", onclick="decrement_weeks_from_now()"),
-        Input(type="number", value=weeks_from_now, id="weeks_from_now", style="width: 4rem; height: 2rem; text-align: center", hx_trigger="change", hx_post="/table", hx_target="#stundenplan"),
+        Input(type="number", value=weeks_from_now, id="weeks_from_now", style="width: 4rem; height: 2rem; text-align: center", hx_trigger="change", hx_post="/table", hx_target="#stundenplan", hx_swap="outerHTML"),
         Button(">", cls="w-6 h-8", onclick="increment_weeks_from_now()"),
         cls="flex justify-center space-x-2"
     )
 
-def WeekTable(weeks_from_now=0):
-    return Div(
-        Navigation(weeks_from_now),
-        render_table(),
-        cls="space-y-4",
-        id="stundenplan"
+
+def TableEntry(start_time: datetime, end_time: datetime, subject: str, room: str):
+    return Td(
+        Div(
+            Span(start_time.strftime("%H:%M"), cls="font-bold"),
+            Span(end_time.strftime(" - %H:%M")),
+            Span(subject, cls="block"),
+            Span(room, cls="block"),
+            cls="space-y-1"
+        ),
     )
+
+def random_table_entry():
+    start_time = datetime.now().replace(hour=random.randint(8, 16), minute=random.randint(0, 59))
+    end_time = start_time.replace(hour=start_time.hour + 1)
+    return TableEntry(start_time, end_time, "Security", "A123")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
